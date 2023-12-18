84d0734a937a3c36998bf8d206c8d25294b91a90
Eduardo Heredia Gonzalez <EduardoHerediaGonzalez@gmail.com>
2023-12-18 15:51:41 -0600

    Files PLATFORM_TYPES_def.h, STD_TYPES_def.h and TMS70LC43x_def.h added to the project.

---
+++ BaseSystem/STD_TYPES_def.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright © 2023 
+ *
+ * @file    STD_TYPES_def.h
+ * @note
+ *        ______________      
+ *        Eduardo
+ *        Heredia Gonzalez
+ *        Diseñador           
+ *
+ * @brief   STD_TYPES_def
+ * @version No oficial
+ *     
+ */
+
+#ifndef STD_TYPES_def_H_
+#define STD_TYPES_def_H_
+
+/* system headers */
+
+/* exported macros */
+#define STD_nZERO       (0)
+#define STD_nLOW        (0)
+#define STD_nOFF        (0)
+#define STD_nIDLE       (0)
+
+#define STD_nONE        (1)
+#define STD_nHIGH       (1)
+#define STD_nON         (1)
+#define STD_nACTIVE     (1)
+
+#define STD_nUNDEFINED  (2)
+
+/* exported types */
+
+#endif /* STD_TYPES_def_H_ */
---
+++ BaseSystem/TMS570LC43x_def.h
@@ -0,0 +1,61 @@
+/**
+ * Copyright © 2023 
+ *
+ * @file    TMS570LC43x_def.h
+ * @note
+ *        ______________      
+ *        Eduardo
+ *        Heredia Gonzalez
+ *        Diseñador           
+ *
+ * @brief   TMS570LC43x_def
+ * @version No oficial
+ *     
+ *     Device Memory Map
+ *
+ *                      Base Address    Size    Description
+ *                      ------------------------------------------------------
+ *                      0x00000000      4MB     Flash
+ *                      0x00400000      NA      Reserved
+ *                      0x08000000      512KB   RAM
+ *                      0x08080000      NA      Reserved
+ *                      0x08400000      512KB   RAM-ECC
+ *                      0x08480000      NA      Reserved
+ *                      0x30000000      64KB    R5F-0 Cache
+ *                      0x34000000      NA      Reserved
+ *                      0x60000000      48MB    EMIF Async RAM
+ *                      0x70000000      NA      Reserved
+ *                      0x80000000      128MB   EMIF SDRAM
+ *                      0xA0000000      NA      Reserved
+ *                      0xF0000000      NA      Flash ECC, OTP & EEPROM
+ *                      0xF0480000      NA      Reserved
+ *                      0xFB000000      512KB   CRC2
+ *                      0xFC000000      NA      Peripherals - Frame 2
+ *                      0xFD000000      NA      Reserved
+ *                      0xFE000000      512KB   CRC1
+ *                      0xFF000000      NA      Peripherals - Frame 3
+ *                      0xFFF80000      NA      SYSTEM Peripherals - Frame 1
+ */
+
+#ifndef TMS570LC43x_def_H_
+#define TMS570LC43x_def_H_
+
+/* system headers */
+
+/* exported macros */
+#define FLASH_nBASE_ADDRESS                         (0x00000000)
+#define RAM_nBASE_ADDRESS                           (0x08000000)
+#define RAM_ECC_nBASE_ADDRESS                       (0x08400000)
+#define R5F0_CACHE_nBASE_ADDRESS                    (0x30000000)
+#define EMIF_ASYNC_RAM_nBASE_ADDRESS                (0x60000000)
+#define EMIF_SDRAM_nBASE_ADDRESS                    (0x80000000)
+#define FLASH_ECC_OTP_EEPROM_nBASE_ADDRESS          (0xF0000000)
+#define CRC2_nBASE_ADDRESS                          (0xFB000000)
+#define PERIPHERALS_FRAME2_nBASE_ADDRESS            (0xFC000000)
+#define CRC1_nBASE_ADDRESS                          (0xFE000000)
+#define PERIPHERALS_FRAME3_nBASE_ADDRESS            (0xFF000000)
+#define SYSTEM_PERIPHERALS_FRAME1_nBASE_ADDRESS     (0xFFF80000)
+
+/* exported types */
+
+#endif /* TMS570LC43x_def_H_ */
